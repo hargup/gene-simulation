9e52f5c0de4a9c30cb8b9fb9b3e2fdab725f717d
Harsh Gupta <gupta.harsh96@gmail.com>
2014-07-07 02:09:38 +0530

    misc improvements

--- simulate.py
+++ simulate.py
@@ -1,6 +1,30 @@
 """ A simulation to check the effect of interbreeding on the percentage of
 ressesive carrier """
 
+# The prime motive for this simulation was to test if interbreeding will
+# eleminate the ressesive but unfavourable gene from the population faster than
+# the exogamy would do. I'm little confused about simulating exogamy and interbreeding
+# and the current program simulates only exogamy. It appears that interbreeding
+# can eliminate unfourable traits pretty fast even if they appear as only ressesive triats
+# so what is left are the traits which are favourable, exogamy appears to have its own
+# advantages by as it can have best of both world mixing the goods two or more not very
+# closely related gene pools. Or maybe a something like a restricted exogamy is
+# favorable which is followed in northern India, where people aren't allowed to marry very
+# closely related people (people of same gotra), but they don't marry outside the caste.
+# Or maybe something like branch out -> grow in isolation -> merge. BTW we(humans) did
+# branched out of early apes and mating with gorrilas (our cousins) doesn't seems to be
+# very good idea.
+#
+# Outbreeding appears to have a better chance at producing good, whereas inbreeding seems to
+# be better at eliminating the bad. I guess there will be a lot of factors at play for example
+# the inbreeding specie will have a lot more unsuccessful kids which may lead to negative population
+# growth and the ultimate extinction of the specie. We have a lot less risk of such cases in
+# outbreeding because there are less chances of producing something too bad but what are the rates
+# of progation of some good trait emerging out of the system? These are some pretty
+# interesting questions.
+
+# XXX: the simulation might not even a good candidate for inbreeding
+
 import numpy as np
 import itertools
 from matplotlib import pyplot as plt
@@ -21,7 +45,7 @@ survival_rates = {
     'XX': 1.0,
     'XY': 1.0,
     'YX': 1.0,
-    'YY': 0.5
+    'YY': 1.0
 }
 
 initial_gene_ratio = {
@@ -82,50 +106,37 @@ def gene_percentage(pop, gene):
     return sum(1 for p in pop if p.gene == gene)/(1.0*len(pop))
 
 
-def play_for_gens(pop_size=500, no_gens=100):
-    pop = gen_population(pop_size)
-
+def play_for_gens(pop_size=500, no_gens=100, times_of_run = 100):
     print("Running the simulations with populuation"
-          "size %s for %s generations " %(pop_size, no_gens))
+          "size %s for %s generations for %s times " %(pop_size, no_gens, times_of_run))
     pop_ratios = {
-        'XX': [gene_percentage(pop, 'XX')],
-        'XY': [gene_percentage(pop, 'XY')],
-        'YX': [gene_percentage(pop, 'YX')],
-        'YY': [gene_percentage(pop, 'YY')]
+        'XX': [[] for i in range(times_of_run)],
+        'XY': [[] for i in range(times_of_run)],
+        'YX': [[] for i in range(times_of_run)],
+        'YY': [[] for i in range(times_of_run)]
     }
-    # Though XY is not YX is not different but ignored it for simplicity
-    for i in xrange(no_gens):
-        pop = next_gen(pop)
-        pop_ratios['XX'].append(gene_percentage(pop, 'XX'))
-        pop_ratios['XY'].append(gene_percentage(pop, 'XY'))
-        pop_ratios['YX'].append(gene_percentage(pop, 'YX'))
-        pop_ratios['YY'].append(gene_percentage(pop, 'YY'))
+    for i in xrange(times_of_run):
+        pop = gen_population(pop_size)
+
+        # Though XY is not YX is not different but ignored it for simplicity
+        for j in xrange(no_gens):
+            for gene in initial_gene_ratio.keys():
+                pop_ratios[gene][i].append(gene_percentage(pop, gene))
+            pop = next_gen(pop)
+
+        for gene in initial_gene_ratio.keys():
+            pop_ratios[gene][i] = np.array(pop_ratios[gene][i])
+
+    for gene in initial_gene_ratio.keys():
+        pop_ratios[gene] = sum(pop_ratios[gene])/times_of_run
 
     return pop_ratios
 
-results = [play_for_gens(pop_size=500), play_for_gens(pop_size=1000), play_for_gens(pop_size=2000)]
-plt.plot(results[0]['YY'], 'r')
-plt.plot(results[1]['YY'], 'g')
+results = [None, None, None]
+
+# survival_rates['YY'] = 0.999
+results[2] = play_for_gens()
+
+plt.plot(results[2]['XY'], 'r')
+plt.plot(results[2]['YX'], 'g')
 plt.plot(results[2]['YY'], 'b')
-
-# plt.plot(result1['XY'], 'r')
-# plt.plot(result1['YX'], 'g')
-# plt.plot(result1['YY'], 'y')
-
-# How will it play out when
-
-# Simulate the process of Exomarriage and gotra and inbreeding
-# and exobreeding, marriage with cousins. Maintaining pedigree, etc
-
-# Here the population size has been kept fixed try increasing it with
-# numbers of generations.
-
-# What about favourable recessive traits? It can be modeled by having a XX
-# have a higher survival percentage than the XY and YY where both have same survial
-# percentage
-
-# Can have a lot of gene sets
-
-
-# Don't lead to conclusions. Give warning about your limited knowledge of
-# genetics
