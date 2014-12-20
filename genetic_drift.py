correct average_results

genetic_drift.py
@@ -3,27 +3,25 @@ Monte Carlo Simulations to study the genetic drift
 
 Model
 -----
-
-There are two species of asexual organisms who produce the indentical copies
-of themselves after each generation. Because of enviornmental factors the total
-number of organisms remain conserved.
+* Non overlaping generations
+* asexual reproduction
+* constant population size
 """
 
 import numpy as np
-import itertools
 from matplotlib import pyplot as plt
-from functools import reduce
+import seaborn as sns
 
 
 class WorldModel():
     survival_rates = {
         'X': 1.0,
-        'Y': 1.0
+        'Y': 1.0,
     }
 
     initial_gene_ratio = {
         'X': 1.0,
-        'Y': 1.0
+        'Y': 1.0,
     }
 
 
@@ -34,15 +32,16 @@ class DefaultArgs:
 
 def gen_population(pop_size):
     """ generate population of pop_size according to the world model"""
-    pop = np.zeros(pop_size, dtype=tuple)
+    pop = np.zeros(pop_size, dtype=object)
 
     i = 0
     for gene in WorldModel.initial_gene_ratio.keys():
-        j = i + int(WorldModel.initial_gene_ratio[gene]*pop_size
-                    /sum(WorldModel.initial_gene_ratio.values()))
-        pop[i:j] = gene
+        j = i + int(WorldModel.initial_gene_ratio[gene] * pop_size
+                    / sum(WorldModel.initial_gene_ratio.values()))
+        pop[i: j] = gene
         i = j
-    assert j == pop_size
+
+    assert j == pop_size  # TODO: there shouldn't be a need to do this
     np.random.shuffle(pop)
     return pop
 
@@ -50,7 +49,7 @@ def gen_population(pop_size):
 def next_gen(pop):
     n = len(pop)
 
-    children = np.zeros(2*n, dtype=tuple)
+    children = np.zeros(2*n, dtype=object)
     children[:n] = pop
     children[n:] = pop
 
@@ -90,28 +89,31 @@ def play_for_gens(pop_size=DefaultArgs.pop_size, no_gens=DefaultArgs.no_gens):
     return pop_ratios
 
 
-def average_results(pop_size=DefaultArgs.pop_size, no_gens=DefaultArgs.no_gens, no_times=10):
+def average_results(pop_size=DefaultArgs.pop_size,
+                    no_gens=DefaultArgs.no_gens, no_times=10):
     """
     Caculates the variation of ratio of the population from the highest to
     the lowest percentage with the number of generations
     """
-    no_genes = WorldModel.initial_gene_ratio.keys())
+    avg = np.array(play_for_gens(pop_size, no_gens).values())
+    avg.sort(axis=0)
+    for i in range(1, no_times):
+        result = np.array(play_for_gens(pop_size, no_gens).values())
+        result.sort(axis=0)
+        avg += result
 
-    pop_ratios = np.zeros(no_genes, dtype=np.object)
-    for i, gene in enumerate(WorldModel.initial_gene_ratio.keys()):
-        pop_ratios.append(np.zeros(no_gens, dtype=np.float16))
+    avg = avg/no_times
 
-    for i in range(no_times):
-        result = play_for_gens(pop_size, no_gens)
-        ratios = np.array(sorted(result.values()))
-        pop_ratios
-        for gene in WorldModel.initial_gene_ratio.keys():
-            pop_ratios[gene] += result[gene]
+    return avg
 
-    for gene in WorldModel.initial_gene_ratio.keys():
-        pop_ratios[gene] = pop_ratios[gene]/no_times
 
-    return pop_ratios
+def plot_ranks(avg):
+    no_ranks, no_gens = avg.shape
+    for i in range(no_ranks):
+        plt.plot(avg[i])
+
+    sns.set_style('darkgrid')
+    plt.axis([0, no_gens, 0, 1.0])
 
 
 def plot_populations(results):
@@ -120,3 +122,6 @@ def plot_populations(results):
     plt.plot(results['Y'], 'r')
     plt.plot(results['X'], 'b')
     plt.axis([0, no_gens, 0, 1.0])
+
+# plt.plot(avg[0], 'r')
+# plt.plot(avg[1], 'b')